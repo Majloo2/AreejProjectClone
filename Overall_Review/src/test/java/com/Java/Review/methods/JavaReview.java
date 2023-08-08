a3d9e617122c1a95f6603491bfd90cd4d76732c8
Majloo2 <141463997+Majloo2@users.noreply.github.com>
2023-08-08 17:06:15 -0400

    Add files via upload
    AreejClone

---
+++ Overall_Review/src/test/java/com/Java/Review/methods/JavaReview.java
@@ -0,0 +1,32 @@
+package com.Java.Review.methods;
+
+public class JavaReview {
+
+	
+	public void JavaVariables() {
+		
+		// String, int, float, char, boolean
+		
+		
+		//String Example
+		String Vehicle = "GMC";
+		System.out.println(Vehicle);
+		
+		// int example
+		int PlayerNumber = 10;
+		System.out.println(PlayerNumber);
+		
+		// final int Example - Unchangeable 
+		final int MyNumb = 15;
+		System.out.println(MyNumb);
+		
+		// float example - Suffix needed after the value
+		float decimal_Number_Primitive_Data = 8000664.10f;
+		System.out.println(decimal_Number_Primitive_Data);
+		
+		// char - stores letters 
+		
+		
+		
+	}
+}
